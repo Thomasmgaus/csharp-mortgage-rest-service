8d796d1590a4521cf76f103f2fda642ed682cfb6
Thomas gaus <thomasmgaus@gmail.com>
2024-03-02 13:00:09 -0500

    Simplifiy the vue app

---
+++ Web/mortgage-frontend/src/components/Mortgage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Here</h1>
+    <div>
+        {{mortgageApplicant}}
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    interface Applicant {
+        name: string,
+        principleAmount: number;
+        annualRate: number;
+        loanYears: 0;
+    }
+
+    const mortgageApplicant: Applicant = ref({name: '', principleAmount: 0, annualRate: 0, loanYears: 0})
+        
+    
+</script>
